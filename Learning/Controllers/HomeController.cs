e3d2e5b94ce99940e3e3bd0444a39e7e66f77af9
Muhammad <danishkanirya9999@gmail.com>
2024-12-25 06:55:24 +0500

    Controller implementation

---
+++ Learning/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Learning.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        } 
+     public ContentResult Not()
+        {
+            return Content("Hey this is danish");
+        } 
+   
+    }
+}
