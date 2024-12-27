bfae1040ff6210619fa9c6dfdf5a27fea6e7d4a1
Muhammad <danishkanirya9999@gmail.com>
2024-12-27 07:21:32 +0500

    login and register

--- Learning/Program.cs
+++ Learning/Program.cs
@@ -1,4 +1,12 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Learning.Data;
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("AuthDbContextConnection") ?? throw new InvalidOperationException("Connection string 'AuthDbContextConnection' not found.");
+
+builder.Services.AddDbContext<AuthDbContext>(options => options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<AuthDbContext>();
 builder.Services.AddControllersWithViews();
 var app = builder.Build();
 app.MapControllerRoute(
